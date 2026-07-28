053be2cdae8790484de346bb212d974eb14a5608
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-28 08:08:49 +0100

    Fix import case for MovieRoutes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
-import MovieRoutes from "./routes/Movie.js";
+import MovieRoutes from "./routes/movie.js";
 
 dotenv.config();
 const app = express();
