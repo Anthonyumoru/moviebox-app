eda1c06711528bc4d25bff344bdbb95bacc74c87
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-28 14:54:01 +0100

    Add Upload component for movie uploads

---
+++ client/src/pages/Upload.jsx
@@ -0,0 +1,8 @@
+export default function Upload() {
+  return (
+    <div className="p-4 text-white">
+      <h1 className="text-2xl">Upload Movie</h1>
+      <p>Upload form will go here</p>
+    </div>
+  );
+}
