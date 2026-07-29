5fa51ff9950ccc8d8c866a63e712404b7f76d555
Anthonyumoru <anthonyumorua@gmail.com>
2026-07-29 11:24:08 +0100

    Add fields to movie schema and update structure

--- server/models/movie.js
+++ server/models/movie.js
@@ -1,13 +1,19 @@
 import mongoose from "mongoose";
 
 const movieSchema = new mongoose.Schema({
+  id: { type: Number, required: true, unique: true },
   title: { type: String, required: true },
-  description: String,
+  year: { type: Number },
+  rating: { type: Number, default: 0 },
+  poster: { type: String }, // this is your image
+  backdrop: { type: String }, // big background image
+  overview: { type: String }, // description
+  videoUrl: { type: String }, // THIS IS FOR YOUR VIDEO
   genre: [String],
-  thumbnail: String,
-  videoUrl: String,
-  duration: Number,
-  status: { type: String, enum: ["pending","approved"], default: "pending" },
+  description: { type: String },
+  thumbnail: { type: String },
+  duration: { type: Number },
+  status: { type: String, enum: ["pending", "approved"], default: "pending" },
   views: { type: Number, default: 0 }
 }, { timestamps: true });
 
